914936612fea9fc9ac4617e685771daca7c72014
Kirti Vashishtha <kirti.vashishtha@enukesoftware.com>
2018-10-10 01:21:42 +0530

    Initials

---
+++ Headletters_PayPalPaymentCheck/PayPalPaymentService.vb
@@ -0,0 +1,28 @@
+﻿Public Class PayPalPaymentService
+    Dim timer As Timers.Timer
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        'System.Diagnostics.Debugger.Launch()
+        timer = New Timers.Timer()
+        timer.Interval = 60000
+        AddHandler timer.Elapsed, AddressOf TriggerPayPayPaymentUPdate
+        timer.Enabled = True
+        Dim strFile As String = String.Format("C:\Astro\ServiceLogs\PayPalPaymentVarification_{0}.txt", DateTime.Today.ToString("ddMMMyyyy"))
+        IO.File.AppendAllText(strFile, String.Format("Service Started at-- {0}{1}", DateTime.Now, Environment.NewLine))
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        timer.Enabled = False
+        Dim strFile As String = String.Format("C:\Astro\ServiceLogs\PayPalPaymentVarification_{0}.txt", DateTime.Today.ToString("ddMMMyyyy"))
+        IO.File.AppendAllText(strFile, String.Format("Service Stopped at-- {0}{1}", DateTime.Now, Environment.NewLine))
+    End Sub
+
+    Private Sub TriggerPayPayPaymentUPdate(obj As Object, e As EventArgs)
+        Try
+            Dim paymntobj As PayPayPaymentUPdate = New PayPayPaymentUPdate()
+            paymntobj.PaymentCheck()
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\Astro\ServiceLogs\PayPalPaymentVarification_ErrorLog_{0}.txt", DateTime.Today.ToString("ddMMMyyyy"))
+            IO.File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        End Try
+    End Sub
+End Class
---
+++ Headletters_PayPalPaymentCheck/ProjectInstaller.Designer.vb
@@ -0,0 +1,46 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class ProjectInstaller
+    Inherits System.Configuration.Install.Installer
+
+    'Installer overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PayPalPaymentInstaller = New System.ServiceProcess.ServiceProcessInstaller()
+        Me.ServiceInstaller1 = New System.ServiceProcess.ServiceInstaller()
+        '
+        'PayPalPaymentInstaller
+        '
+        Me.PayPalPaymentInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem
+        Me.PayPalPaymentInstaller.Password = Nothing
+        Me.PayPalPaymentInstaller.Username = Nothing
+        '
+        'ServiceInstaller1
+        '
+        Me.ServiceInstaller1.ServiceName = "Headletters_PayPalPaymentService"
+        Me.ServiceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic
+        '
+        'ProjectInstaller
+        '
+        Me.Installers.AddRange(New System.Configuration.Install.Installer() {Me.PayPalPaymentInstaller, Me.ServiceInstaller1})
+
+    End Sub
+
+    Friend WithEvents PayPalPaymentInstaller As ServiceProcess.ServiceProcessInstaller
+    Friend WithEvents ServiceInstaller1 As ServiceProcess.ServiceInstaller
+End Class
